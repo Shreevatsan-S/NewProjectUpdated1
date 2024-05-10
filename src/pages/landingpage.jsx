b134a21d87e68aae9b1622f9ba573e57dbdad1b4
Shreevatsan S S <133779156+Shreevatsan-S@users.noreply.github.com>
2024-05-10 23:45:06 +0530

    loginFix2

--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -19,7 +19,7 @@ const toggleElem = (elem) => {
 
 const handleMessage = (event) => {
     event.preventDefault();
-
+   console.log(event.data);
     if (event.data === 'loginClicked') {
         window.open("https://rajaeduacademy.vercel.app/login", "_self");
     }
